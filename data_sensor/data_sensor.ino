52ebe68d142738b507ad395b69d20da64b597ebe
FreakJazz <jazzrb2307@hotmail.com>
2022-01-31 10:06:39 -0500

    add bluetooth and i2c config

--- data_sensor/data_sensor.ino
+++ data_sensor/data_sensor.ino
@@ -2,15 +2,15 @@
 #define SLAVE_ADDRESS 0x40 // Define the I2C address to Communicate to Uno
 
 byte response[2]; // this data is sent to PI
-volatile short LDR_value; // Global Declaration
-const int LDR_pin=A3; //pin to which LDR is connected A0 is analog A0 pin  
+volatile short value; // Global Declaration
+const int analog = A3; //pin to which LDR is connected A0 is analog A0 pin  
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
   Wire.begin(SLAVE_ADDRESS); // this will begin I2C Connection with 0x40 address
   Wire.onRequest(sendData); // sendData is funtion called when Pi requests data
-  pinMode(LDR_pin,INPUT);
+  pinMode(analog,INPUT);
 }
 
 void loop() {
@@ -19,9 +19,9 @@ void loop() {
 delay(1000);
 }
 void sendData(){
-  LDR_value=analogRead(LDR_pin);
+  value=analogRead(analog);
   // Arduino returns 10bit data but we need to convert it to 8bit 
-  LDR_value=map(LDR_value,0,1023,0,255);
-  response[0]=(byte)LDR_value;
+  value=map(value,0,1023,0,255);
+  response[0]=(byte)value;
   Wire.write(response,2); // return data to PI
 }
