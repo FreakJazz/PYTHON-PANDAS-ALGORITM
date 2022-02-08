012709118a9ebc182f4e7f605d1ffa6ef6fed725
FreakJazz <jazzrb2307@hotmail.com>
2022-02-08 18:30:08 -0500

    add new data

--- data_sensor/data_sensor.ino
+++ data_sensor/data_sensor.ino
@@ -2,15 +2,15 @@
 #define SLAVE_ADDRESS 0x40 // Define the I2C address to Communicate to Uno
 
 byte response[2]; // this data is sent to PI
-volatile short value; // Global Declaration
-const int analog = A3; //pin to which LDR is connected A0 is analog A0 pin  
+volatile short LDR_value; // Global Declaration
+const int LDR_pin=A3; //pin to which LDR is connected A0 is analog A0 pin  
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
   Wire.begin(SLAVE_ADDRESS); // this will begin I2C Connection with 0x40 address
   Wire.onRequest(sendData); // sendData is funtion called when Pi requests data
-  pinMode(analog,INPUT);
+  pinMode(LDR_pin,INPUT);
 }
 
 void loop() {
@@ -19,9 +19,10 @@ void loop() {
 delay(1000);
 }
 void sendData(){
-  value=analogRead(analog);
+  LDR_value=analogRead(LDR_pin);
   // Arduino returns 10bit data but we need to convert it to 8bit 
-  value=map(value,0,1023,0,255);
-  response[0]=(byte)value;
+  LDR_value=map(LDR_value,0,1023,0,255);
+  response[0]=(byte)LDR_value;
   Wire.write(response,2); // return data to PI
+  Serial.println("ok");
 }
